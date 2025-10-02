377125e570fa61fccd2de9ef67f548568dee4493
Ekaterina Tkachenko <etkachenko.ok@gmail.com>
2025-10-01 22:23:38 -0400

    task config

---
+++ task-service/src/main/java/com/sayless/task/TaskServiceApp.java
@@ -0,0 +1,12 @@
+package com.sayless.task;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskServiceApp {
+    public static void main(String[] args) {
+        SpringApplication.run(TaskServiceApp.class, args);
+
+    }
+    
+}
