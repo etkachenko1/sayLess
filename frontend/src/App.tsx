c0f9ffb0cb26452f6e001a22f35512a8ff50b569
Ekaterina Tkachenko <etkachenko.ok@gmail.com>
2025-10-06 21:16:23 -0400

    added dashboard route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,16 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 import Auth from "./Auth"
+import Dashboard from "./pages/Dashboard"
 
 function App() {
-  return <Auth />
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path= "/" element = {<Auth />}/>
+      <Route path= "/dashboard" element = {<Dashboard/>}/>
+    </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
