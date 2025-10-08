fbff29e5d4afc9b81d117bd0561a0e8a667b8a60
Ekaterina Tkachenko <etkachenko.ok@gmail.com>
2025-10-08 17:34:47 -0400

    http bridge between task and authservice to get username

---
+++ task-service/src/main/java/com/sayless/task/client/UserClient.java
@@ -0,0 +1,24 @@
+package com.sayless.task.client;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import java.util.Map;
+
+@Component
+public class UserClient {
+    private final RestTemplate restTemplate = new RestTemplate();
+    private static final String AUTH_URL = "http://localhost:8081/users/";
+
+    public String getUsername(String userId) {
+        if(userId == null) return null;
+        try {
+            Map user = restTemplate.getForObject(AUTH_URL + userId, Map.class);
+            if(user!=null && user.get("username")!=null)
+                return user.get("username").toString();
+        }
+        catch (Exception e) {
+            System.out.println("Failed to fetch username");
+        }
+        return "unknown";
+    }
+    
+}
