63125a07ecbf98b6c499dccf8155888620d2bf34
Ekaterina Tkachenko <etkachenko.ok@gmail.com>
2026-07-13 17:29:07 -0400

    fix logger and event getter

--- task-service/src/main/java/com/sayless/task/kafka/TaskEventProducer.java
+++ task-service/src/main/java/com/sayless/task/kafka/TaskEventProducer.java
@@ -3,6 +3,8 @@ package com.sayless.task.kafka;
 import com.sayless.task.event.TaskCreatedEvent;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Service
 public class TaskEventProducer {
@@ -10,6 +12,7 @@ public class TaskEventProducer {
     private static final String TOPIC = "task-created";
 
     private final KafkaTemplate<String, TaskCreatedEvent> kafkaTemplate;
+    private static final Logger log = LoggerFactory.getLogger(TaskEventProducer.class);
 
     public TaskEventProducer(KafkaTemplate<String, TaskCreatedEvent> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
@@ -19,7 +22,7 @@ public class TaskEventProducer {
         System.out.println("Publishing event: " + event.getTitle());
 
         kafkaTemplate.send(TOPIC, event.getTaskId(), event);
-        System.out.println("After send");
+        log.info("Published task-event-created event {}", event.getTaskId());
 
     }
    
