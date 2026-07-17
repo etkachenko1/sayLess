15905b8068ef7b3d581dec4e3559ab97df5897e3
Ekaterina Tkachenko <etkachenko.ok@gmail.com>
2026-07-16 23:58:06 -0400

    secutiry added into notification service

---
+++ notification-service/src/main/java/com/sayless/notification/security/JwtVerifier.java
@@ -0,0 +1,31 @@
+package com.sayless.notification.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.Jwts;
+import java.security.Key;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+//https://www.viralpatel.net/java-create-validate-jwt-token/
+// helper for parsing and validating JWT tokens
+
+@Component
+public class JwtVerifier {
+    private final Key key;
+
+    //constructor to read from app.props or .env in future, converts it to a Key object using Keys.hmacShaKeyFor
+    public JwtVerifier(@Value("${jwt.secret}") String secret){
+        this.key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public Claims parse(String token) {
+        return Jwts.parserBuilder().setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    
+}
---
+++ notification-service/src/main/java/com/sayless/notification/security/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.sayless.notification.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod; 
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity; 
+import org.springframework.security.web.SecurityFilterChain; 
+import org.springframework.security.config.annotation.web.builders.HttpSecurity; 
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter; 
+import org.springframework.web.cors.CorsConfiguration; 
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http, JwtAuthFilter jwtFilter) throws Exception {
+        http.csrf(csrf->csrf.disable())
+            .cors(cors->cors.configurationSource(req -> {
+                CorsConfiguration c = new CorsConfiguration();
+                c.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:3000"));
+                c.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+                c.setAllowedHeaders(List.of("*"));
+                c.setAllowCredentials(true);
+                return c;
+
+            })).formLogin(form->form.disable()).httpBasic(basic->basic.disable())
+            //disable default spring security login and basic auth since we have our own
+            //no auth required for spring health check, error handler and OPTIONS reqs from cors preflight
+            .authorizeHttpRequests(auth->auth
+            .requestMatchers("/actuator/health", "/error").permitAll()
+            .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+            .anyRequest().authenticated()
+            )
+            //signaling touse custom filter instead of spring security chain's default one
+            .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+            
+            return http.build();
+    }
+    
+    
+}
