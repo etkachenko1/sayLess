fe804a9a2c9104b0d149b76154f35fe6ac90bc94
Ekaterina Tkachenko <etkachenko.ok@gmail.com>
2026-08-16 02:10:36 -0400

    fix Kafka consumer failing to deserialize Instant fields in task events

---
+++ notification-service/src/main/java/com/sayless/notification/config/KafkaConsumerConfig.java
@@ -0,0 +1,26 @@
+package com.sayless.notification.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.boot.kafka.autoconfigure.DefaultKafkaConsumerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+@Configuration
+public class KafkaConsumerConfig {
+
+    @Bean
+    public DefaultKafkaConsumerFactoryCustomizer javaTimeAwareJsonDeserializer() {
+        return this::configureValueDeserializer;
+    }
+
+    @SuppressWarnings("unchecked")
+    private void configureValueDeserializer(DefaultKafkaConsumerFactory<?, ?> consumerFactory) {
+        ObjectMapper objectMapper = new ObjectMapper().registerModule(new JavaTimeModule());
+        JsonDeserializer<Object> valueDeserializer = new JsonDeserializer<>(objectMapper);
+        valueDeserializer.trustedPackages("*");
+        ((DefaultKafkaConsumerFactory<Object, Object>) consumerFactory).setValueDeserializer(valueDeserializer);
+    }
+}
